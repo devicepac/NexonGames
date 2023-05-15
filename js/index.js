ca9344924eabf0dd4663b5f19f6491656bbb7550
devicepac <bardichedevice@gmail.com>
2023-05-15 19:05:34 +0900

    feat: 게임, 인트로덕스 슬라이드 효과 추가 완료, 뉴스 작업중

--- js/index.js
+++ js/index.js
@@ -10,12 +10,21 @@ window.onload = function () {
 
   let mySwiper = new Swiper(".sw-game", {
     spaceBetween: 0,
+    loop: true,
     autoplay: {
       delay: 16000,
       disableOnInteraction: false,
     },
+    navigation: {
+      nextEl: ".game-slide .sw-next",
+    },
   });
   let mySwiper2 = new Swiper(".sw-introduce", {
     spaceBetween: 0,
+    loop: true,
+    navigation: {
+      nextEl: ".introduce .sw-next",
+      prevEl: ".introduce .sw-prev",
+    },
   });
 };
