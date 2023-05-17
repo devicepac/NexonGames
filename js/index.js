f8198cbcc4a90b5c845f55201c4e9f0ba5954ac8
devicepac <bardichedevice@gmail.com>
2023-05-17 13:00:27 +0900

    feat: fix-banner 추가 footer-down작업

--- js/index.js
+++ js/index.js
@@ -1,4 +1,15 @@
 window.onload = function () {
+  // 위로 이동하기
+  // .gotop 을 js에 저장하자.
+  const goTop = document.querySelector(".fix-banner");
+  goTop.addEventListener("click", function () {
+    // 위로 슬라이등 코드
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+  
   let header = document.querySelector(".header");
   let nav = document.querySelector(".header-nav");
   nav.addEventListener("mouseenter", function () {
