9869ed8c630baca25c3b342031dd80ae6c3be5bd
devicepac <bardichedevice@gmail.com>
2023-05-18 18:34:39 +0900

    feat: header css 반응형 작업

--- js/index.js
+++ js/index.js
@@ -11,7 +11,48 @@ window.onload = function () {
   });
 
   // 모바일 버튼 기능
+  const menuToggle = document.querySelector(".header-hidden-icon");
+  const mobileMenu = document.querySelector(".mobile-menu");
+  const closeMenuButton = document.querySelector(".mobile-close-bt");
 
+  menuToggle.addEventListener("click", function () {
+    mobileMenu.style.right = "0"; /* 메뉴가 오른쪽에서 나오도록 위치 조정 */
+  });
+
+  closeMenuButton.addEventListener("click", function () {
+    mobileMenu.style.right = "-100%"; /* 메뉴를 오른쪽으로 숨김 */
+  });
+
+  // 모바일 메뉴
+  const menuItems = document.querySelectorAll(".mobile-gnb > li");
+
+  menuToggle.addEventListener("click", function () {
+    mobileMenu.classList.toggle("open"); /* 모바일 메뉴를 열거나 닫음 */
+  });
+
+  menuItems.forEach(function (item) {
+    item.addEventListener("click", function (event) {
+      const subMenu = item.querySelector(".mobile-submenu");
+      if (subMenu) {
+        event.preventDefault(); /* 기본 클릭 동작을 막음 */
+        const isOpen = subMenu.style.display === "block";
+        closeAllSubMenus(); /* 다른 모든 서브메뉴를 닫음 */
+
+        if (!isOpen) {
+          subMenu.style.display = "block"; /* 클릭한 항목의 서브메뉴를 나타냄 */
+        } else {
+          subMenu.style.display = "none"; /* 클릭한 항목의 서브메뉴를 숨김 */
+        }
+      }
+    });
+  });
+
+  function closeAllSubMenus() {
+    const subMenus = document.querySelectorAll(".mobile-submenu");
+    subMenus.forEach(function (subMenu) {
+      subMenu.style.display = "none"; /* 모든 서브메뉴를 숨김 */
+    });
+  }
   // 헤더 메뉴
   let header = document.querySelector(".header");
   let nav = document.querySelector(".header-nav");
@@ -22,6 +63,7 @@ window.onload = function () {
     header.classList.remove("header-active");
   });
 
+  //게임 슬라이드
   let mySwiper = new Swiper(".sw-game", {
     spaceBetween: 0,
     loop: true,
@@ -33,16 +75,23 @@ window.onload = function () {
       nextEl: ".game-slide .bt-next",
     },
     pagination: {
-      el: '.sw-number',
-      type: 'custom',
+      el: ".sw-number",
+      type: "custom",
       clickable: true,
       renderCustom: function (swiper, current, total) {
-        let currentIndex = ('0' + current).slice(-2);
-        let totalSlides = ('0' + total).slice(-2);
-        return '<div class="swiper-o-number">' + currentIndex + '</div><span>&nbsp;/&nbsp;</span><span>' + totalSlides + '</span>';
+        let currentIndex = ("0" + current).slice(-2);
+        let totalSlides = ("0" + total).slice(-2);
+        return (
+          '<div class="swiper-o-number">' +
+          currentIndex +
+          "</div><span>&nbsp;/&nbsp;</span><span>" +
+          totalSlides +
+          "</span>"
+        );
       },
     },
   });
+  //설명문 슬라이드
   let mySwiper2 = new Swiper(".sw-introduce", {
     spaceBetween: 0,
     loop: true,
